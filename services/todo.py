2a5aed7bf476f3b49383abf9284cfbe522ac6fec
Mounika <mounika@nuevesolutions.com>
2020-11-05 06:43:37 +0000

    Added init

--- services/todo.py
+++ services/todo.py
@@ -3,27 +3,25 @@ import json
 # import db
 from db import db
 
-items = []
-
 
 class TodoItems(RequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db_instance = db()
+        self.dbconn = self.db_instance.conn
+        self.dbcursor = self.db_instance.cursor
+
     def get(self):
-        db_instance = db()
-        dbconn = db_instance.conn
-        dbcursor = db_instance.cursor
-        dbcursor.execute("SELECT * FROM todo")
-        todo_records = dbcursor.fetchall()
+        self.dbcursor.execute("SELECT * FROM todo")
+        todo_records = self.dbcursor.fetchall()
         items = [{'id': row[0], 'title': row[1]}for row in todo_records]
         # todo_records format [(1, 'task-1'), (1, 'task-2'), (2, 'do HW')]
-        dbconn.commit()
+        self.dbconn.commit()
         self.write({'items': items})
 
     def post(self):
         data = json.loads(self.request.body.decode('ascii'))
-        db_instance = db()
-        dbconn = db_instance.conn
-        dbcursor = db_instance.cursor
-        dbcursor.execute(
+        self.dbcursor.execute(
             "INSERT INTO todo VALUES({0}, '{1}')". format(data['id'], data['title']))
-        dbconn.commit()
+        self.dbconn.commit()
         self.write({'item': json.loads(self.request.body.decode('ascii'))})
